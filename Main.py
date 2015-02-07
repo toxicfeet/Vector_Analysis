c37d499e89e0d5381bb2316b3e1becc3b5e5bc60
toxicfeet <dziliak@gmail.com>
2015-02-06 20:53:06 -0600

    Working on GUI

--- Main.py
+++ Main.py
@@ -67,20 +67,20 @@ while True:
         if vector_list[0] == '1':
             del vector_list[0]
             cart = [float(x.strip()) for x in vector_list]
-            cyl = Convert.cart_to_cyl(cart[0], cart[1], cart[2])
-            sph = Convert.cart_to_sph(cart[0], cart[1], cart[2])
+            cyli = Convert.cart_to_cyl(cart[0], cart[1], cart[2])
+            sphe = Convert.cart_to_sph(cart[0], cart[1], cart[2])
             break
         elif vector_list[0] == '2':
             del vector_list[0]
-            cyl = [float(x.strip()) for x in vector_list]
-            cart = Convert.cyl_to_cart(cyl[0], cyl[1], cyl[2])
-            sph = Convert.cyl_to_sph(cyl[0], cyl[1], cyl[2])
+            cyli = [float(x.strip()) for x in vector_list]
+            cart = Convert.cyl_to_cart(cyli[0], cyli[1], cyli[2])
+            sphe = Convert.cyl_to_sph(cyli[0], cyli[1], cyli[2])
             break
         elif vector_list[0] == '3':
             del vector_list[0]
-            sph = [float(x.strip()) for x in vector_list]
-            cart = Convert.sph_to_cart(sph[0], sph[1], sph[2])
-            cyl = Convert.sph_to_cyl(sph[0], sph[1], sph[2])
+            sphe = [float(x.strip()) for x in vector_list]
+            cart = Convert.sph_to_cart(sphe[0], sphe[1], sphe[2])
+            cyli = Convert.sph_to_cyl(sphe[0], sphe[1], sphe[2])
             break
         elif vector_list[0] == 'q' or 'Q' or 'quit' or 'QUIT':
             print('Thank you for using the Vector Simplification Program.')
@@ -93,9 +93,9 @@ while True:
                                                                                         'Spherical'))
     print()
     print('Cartesian:   (' + "%.4f" % cart[0] + ', ' + "%.4f" % cart[1] + ', ' + "%.4f" % cart[2] + ').')
-    print('Cylindrical: (' + "%.4f" % cyl[0] + ', ' + "%.4f" % cyl[1] + ', ' + "%.4f" % cyl[2] + ').')
-    print('Spherical:   (' + "%.4f" % sph[0] + ', ' + "%.4f" % sph[1] + ', ' + "%.4f" % sph[2] + ').')
+    print('Cylindrical: (' + "%.4f" % cyli[0] + ', ' + "%.4f" % cyli[1] + ', ' + "%.4f" % cyli[2] + ').')
+    print('Spherical:   (' + "%.4f" % sphe[0] + ', ' + "%.4f" % sphe[1] + ', ' + "%.4f" % sphe[2] + ').')
     print()
     cart = [0, 0, 0]
-    cyl = [0, 0, 0]
-    sph = [0, 0, 0]
+    cyli = [0, 0, 0]
+    sphe = [0, 0, 0]
